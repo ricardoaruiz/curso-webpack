2a490c4f95709e6a81f587ca0874e55d3986a960
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-09-13 09:41:57 -0300

    refactor: adjust on Button component

--- 2_Loaders/src/components/Button/Button.js
+++ 2_Loaders/src/components/Button/Button.js
@@ -1,26 +1,38 @@
 import './Button.scss'
 
-export class Button {
+const createButtonContainer = () => {
+    const buttonContainer = document.createElement('div')
+    buttonContainer.classList.add('button-container')
+    return buttonContainer
+}
 
-    render(label, image) {
-        const buttonContainer = document.createElement('div')
-        buttonContainer.classList.add('button-container')
-
-        if (image) {
-            const buttonImage = document.createElement('img')
-            buttonImage.src = image
-            buttonImage.classList.add('button-img')
-            buttonContainer.appendChild(buttonImage)
-        }
-
-        const buttonLabel = document.createElement('div')
-        buttonLabel.innerText = label
-        buttonContainer.appendChild(buttonLabel)
-
-        const button = document.createElement('button')
-        button.classList.add('button')
-        button.appendChild(buttonContainer)
-        document.querySelector('body').appendChild(button)
+const addImage = (container, image) => {
+    if (image) {
+        const buttonImage = document.createElement('img')
+        buttonImage.src = image
+        buttonImage.classList.add('button-img')
+        container.appendChild(buttonImage)
     }
+}
 
+const addLabel = (container, label) => {
+    const buttonLabel = document.createElement('div')
+    buttonLabel.innerText = label
+    container.appendChild(buttonLabel)
+}
+
+const createButton = (container) => {
+    const button = document.createElement('button')
+    button.classList.add('button')
+    button.appendChild(container)
+    document.querySelector('body').appendChild(button)
+}
+
+export class Button {
+    render(label, image) {
+        const buttonContainer = createButtonContainer()
+        addImage(buttonContainer, image)
+        addLabel(buttonContainer, label)
+        createButton(buttonContainer)
+    }
 }
