e8fdc75655c1dcd0575ec5409e2378135f8e374d
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-09-13 21:47:32 -0300

    feat: creating plugins project and add mini-css-extract-plugin and sass-loader

---
+++ 3_Plugins/src/index.js
@@ -0,0 +1,4 @@
+import { Header } from './components'
+
+const header = new Header()
+header.render('Webpack')
---
+++ 3_Plugins/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
+    ]    
+}
