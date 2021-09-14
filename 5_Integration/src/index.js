17745ca784c94c9f6370ce414de62e5eb1c3e471
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-09-14 14:32:44 -0300

    adding jQuery

--- 5_Integration/src/index.js
+++ 5_Integration/src/index.js
@@ -1,4 +1,9 @@
+import $ from 'jquery'
 import { Header } from './components'
 
 const header = new Header()
 header.render('Webpack Integrate with external libs')
+
+const body = $('body')
+const p = $('<p></p>').text('Inserindo texto').css('color','red')
+body.append(p)
