ed5efbcffb75dffbdb972523768d89df3c4e7c45
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-09-18 14:42:03 -0300

    feat: add lazy loading

---
+++ 8_Advanced_Features_Shimming/src/lazy.js
@@ -0,0 +1,5 @@
+const chamadaAtrasada = () => {
+    console.log('Eu vim do lazy loading')
+}
+
+export default chamadaAtrasada
--- 8_Advanced_Features_Shimming/src/teste.js
+++ 8_Advanced_Features_Shimming/src/teste.js
@@ -2,5 +2,15 @@ console.log('opa')
 
 console.log('TESTE - the difference between informed arrays is', _.difference([1,2,3], [1,4,5]))
 
-$('body').css('background-color','red')
+$('body').css('background-color','blue')
+$('body').css('color','white')
 $('body').append('<p>Adicionado pelo teste</p>')
+
+const btn = $('<button/>')
+    .text('Clique aqui novos')
+    .click(e => import('./lazy').then(module => {
+        const lazy = module.default
+        lazy();
+    }))
+    
+$('body').append(btn)
