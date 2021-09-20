8fb810f5a56554aaf6b5f231d636cf59de84b29f
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-09-20 10:26:45 -0300

    feat: ModuleFederation - creating Home project

---
+++ 9_Module_Federation/packages/contact/src/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Contact.scss'
+
+const Contact = () => (
+    <div className="container">
+        <h2>Aqui é a página de contato</h2>
+        <p>Essa página está em outro App!</p>
+    </div>
+)
+
+export default Contact
