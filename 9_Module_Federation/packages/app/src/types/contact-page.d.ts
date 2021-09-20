df293de583bbe02c203fe3ec423d0e2c6049f2f3
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-09-20 15:32:47 -0300

    feat: removing wrong folder

--- 9_Module_Federation/packages/app/src/types/contact-page.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'ContactApp/ContactPage'
--- 9_Module_Federation/packages/contact/src/bootstrap.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Contact from './Contact'
-
-ReactDOM.render(<Contact />, document.querySelector('#root'))
