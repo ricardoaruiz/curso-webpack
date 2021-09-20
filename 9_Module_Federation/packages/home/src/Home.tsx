8c67ca253f7a1fa7a5b431870b9a95cb9519caaf
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-09-20 10:15:17 -0300

    feat: ModuleFederation - create Home app

---
+++ 9_Module_Federation/packages/home/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Home.scss'
+
+const Home = () => (
+    <div className="jumbotron">
+        <h1>Olá Module Federation</h1>
+        <p>Este componente é de outro App!</p>
+        <button className="button">Botão</button>
+    </div>
+)
+
+export default Home
---
+++ 9_Module_Federation/packages/home/webpack.development.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'http://localhost:9002/'
+    },
+    mode: 'development',
+    devServer: {
+        port: 9002,
+        hot: true,
+        open: true,
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: require.resolve('babel-loader'),
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                use: ['ts-loader'],
+                exclude: /node-modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, './public/index.html'),
+            inject: 'body',
+            title: 'App'
+        })
+    ],
+    optimization: {
+        runtimeChunk: true
+    }
+}
